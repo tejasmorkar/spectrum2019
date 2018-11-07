5eaca1e0d49e87c09b36c878f66ba5e467c20e83
Mrunali Morkar <tejasmorkar@gmail.com>
2018-11-07 21:35:29 +0530

    Changes

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -2,91 +2,17 @@ const bodyContainer = document.getElementById("body-container");
 const footer = document.getElementById("footer");
 const nav = document.getElementById("navigation");
 const downIcon = document.getElementById("down");
-const crossIcon = document.getElementById("cross");
 const theEvent = document.getElementById("nav1");
 const register = document.getElementById("nav2");
 const memos = document.getElementById("nav3");
 const locateUs = document.getElementById("nav4");
 const contactUs = document.getElementById("nav5");
 const theEventPage = document.getElementById("the-event");
-const registrationPage = document.getElementById("registration-form");
 const memosPage = document.getElementById("memories");
 const locateUsPage = document.getElementById("locate-us");
 const contactUsPage = document.getElementById("contact-us");
 
 AOS.init();
-
-crossIcon.classList.add("no-display");
-nav.classList.add("no-display");
-registrationPage.classList.add("no-display");
-memosPage.classList.add("no-display");
-locateUsPage.classList.add("no-display");
-contactUsPage.classList.add("no-display");
-
-
-downIcon.addEventListener("click", () => {
-    downIcon.classList.add("no-display");
-    bodyContainer.classList.add("no-display");
-    theEventPage.classList.add("no-display");
-    registrationPage.classList.add("no-display");
-    memosPage.classList.add("no-display");
-    locateUsPage.classList.add("no-display");
-    contactUsPage.classList.add("no-display");
-    crossIcon.classList.remove("no-display");
-    nav.classList.remove("no-display");
-})
-
-crossIcon.addEventListener("click", () => {
-    crossIcon.classList.add("no-display");
-    nav.classList.add("no-display");
-    downIcon.classList.remove("no-display");
-    bodyContainer.classList.remove("no-display");
-
-})
-
-theEvent.addEventListener("click", () => {
-    crossIcon.classList.add("no-display");
-    nav.classList.add("no-display");
-    downIcon.classList.remove("no-display");
-    bodyContainer.classList.remove("no-display"); 
-    theEventPage.classList.remove("no-display");
-})
-
-register.addEventListener("click", () => {
-    crossIcon.classList.add("no-display");
-    nav.classList.add("no-display");
-    theEventPage.classList.add("no-display");
-    downIcon.classList.remove("no-display");
-    bodyContainer.classList.remove("no-display");
-    registrationPage.classList.remove("no-display");
-})
-
-memos.addEventListener("click", () => {
-    crossIcon.classList.add("no-display");
-    nav.classList.add("no-display");
-    theEventPage.classList.add("no-display");
-    downIcon.classList.remove("no-display");
-    bodyContainer.classList.remove("no-display");
-    memosPage.classList.remove("no-display");
-})
-
-locateUs.addEventListener("click", () => {
-    crossIcon.classList.add("no-display");
-    nav.classList.add("no-display");
-    theEventPage.classList.add("no-display");
-    downIcon.classList.remove("no-display");
-    bodyContainer.classList.remove("no-display");
-    locateUsPage.classList.remove("no-display");
-})
-
-contactUs.addEventListener("click", () => {
-    crossIcon.classList.add("no-display");
-    nav.classList.add("no-display");
-    theEventPage.classList.add("no-display");
-    downIcon.classList.remove("no-display");
-    bodyContainer.classList.remove("no-display");
-    contactUsPage.classList.remove("no-display");
-})
 
 new TypeIt('#intro-text', {
     speed: 100,
@@ -109,7 +35,48 @@ new TypeIt('#intro-text', {
     .type(`And Much More`)
     .pause(1000)
     .delete(13)
-    .type(`The All Round College Fest! </span>`);
+    .type(`The All Round College Fest! </span>`)
+    .break()
+    .type(`<img id="down" src="images/down.png" alt="">`)
+    .destroy();
+
+function formEvent() {
+    const win = window.open('https://goo.gl/forms/kpcTU4AUaoKSZoDm2', '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+}
+
+function gmapLink() {
+    const win = window.open('https://goo.gl/forms/kpcTU4AUaoKSZoDm2', '_blank');
+    if (win) {
+        //Browser has allowed it to be opened
+        win.focus();
+    } else {
+        //Browser has blocked it
+        alert('Please allow popups for this website');
+    }
+}
+
+// var mymap = L.map('mapid').setView([18.653000, 73.762356], 16);
+
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox.streets',
+//     accessToken: 'pk.eyJ1IjoidGVqYXNtb3JrYXIiLCJhIjoiY2pvNnE5d25yMGp0djNxb2F4bTl2MnRhaSJ9.aGAGg5ZR85LE-0DVpqNDsg'
+// }).addTo(mymap);
+
+// var markerClg = L.marker([18.653000, 73.762356]).addTo(mymap);
+
+// var popup = L.popup()
+// .setLatLng([18.648900, 73.764699])
+// .setContent("<b>Akurdi Railway Station</b>")
+// .openOn(mymap);
 
 
 particlesJS.load('particles-js', '../particlesjs-config.json', function () {
